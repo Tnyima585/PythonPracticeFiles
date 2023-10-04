3622b5ea4c6e64376bca1094ce2326c906182e0b
Tenzin Nyima <tnyima585@gmail.com>
2023-10-04 23:32:33 +0530

    revised code sh: line 1: q: command not found

--- for_loop.py
+++ for_loop.py
@@ -7,3 +7,15 @@ for i in name2:
     print(i)
     if i=="Ram":
         print("Where is Sita")
+
+name = "Jenny"
+for who in name:
+    print(who)
+
+name3 = ["Tenzin", "Bhaga", "Nyima"]
+for y in name3:
+    print(y)
+    if y=="Bhaga":
+        print("Where are your family in Tibet")
+    else:
+        print("Move forward")
